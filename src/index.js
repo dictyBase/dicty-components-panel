334273594174cd2d77784e6741e856169bb43adb
Rudy <rudydeberry@hotmail.com>
2017-04-06 00:48:43 -0500

    fix module.exports

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import PanelGroup from './Components/PanelGroup'
+import Panel from './Components/Panel'
+import PanelHeader from './Components/PanelHeader'
+import PanelTitle from './Components/PanelTitle'
+import PanelBody from './Components/PanelBody'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
 
-export PanelGroup from './Components/PanelGroup'
-export Panel from './Components/Panel'
-export PanelHeader from './Components/PanelHeader'
-export PanelTitle from './Components/PanelTitle'
-export PanelBody from './Components/PanelBody'
+
+module.exports = {
+    PanelGroup,
+    Panel,
+    PanelHeader,
+    PanelTitle,
+    PanelBody
+}
