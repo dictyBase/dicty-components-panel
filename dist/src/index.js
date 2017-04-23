45f55ec0fc808c37de7d4aa6c707ac21d042e0a4
Rudy <rudydeberry@hotmail.com>
2017-04-23 18:13:59 -0500

    add babel to dev dependencies

---
+++ dist/src/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PanelBody = exports.PanelTitle = exports.PanelHeader = exports.Panel = exports.PanelGroup = undefined;
+
+var _PanelGroup = require('./Components/PanelGroup');
+
+var _PanelGroup2 = _interopRequireDefault(_PanelGroup);
+
+var _Panel = require('./Components/Panel');
+
+var _Panel2 = _interopRequireDefault(_Panel);
+
+var _PanelHeader = require('./Components/PanelHeader');
+
+var _PanelHeader2 = _interopRequireDefault(_PanelHeader);
+
+var _PanelTitle = require('./Components/PanelTitle');
+
+var _PanelTitle2 = _interopRequireDefault(_PanelTitle);
+
+var _PanelBody = require('./Components/PanelBody');
+
+var _PanelBody2 = _interopRequireDefault(_PanelBody);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.PanelGroup = _PanelGroup2.default;
+exports.Panel = _Panel2.default;
+exports.PanelHeader = _PanelHeader2.default;
+exports.PanelTitle = _PanelTitle2.default;
+exports.PanelBody = _PanelBody2.default;
