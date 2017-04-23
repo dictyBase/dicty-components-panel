0b5c76d2c84bf563ac5cd66988ce506006b169bb
Rudy <rudydeberry@hotmail.com>
2017-04-23 17:04:16 -0500

    hopefully fix export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
 import PanelGroup from './Components/PanelGroup'
 import Panel from './Components/Panel'
 import PanelHeader from './Components/PanelHeader'
